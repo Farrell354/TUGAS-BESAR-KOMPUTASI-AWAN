f494a4ffe3fdae5f3d64993d7908f866d0039f74
farelmario <email@farelmario.com>
2025-12-22 13:49:04 +0700

    Menambahkan startup script untuk Azure

--- startup.sh
+++ startup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Salin config nginx default ke backup
+# 1. Backup config asli
 cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
 
-# Update root folder ke /public (Wajib untuk Laravel)
+# 2. Arahkan root ke folder public
 sed -i 's|root /home/site/wwwroot;|root /home/site/wwwroot/public;|g' /etc/nginx/sites-available/default
 
-# Tambahkan index.php ke daftar index
+# 3. Tambahkan index.php
 sed -i 's|index index.php index.html index.htm;|index index.php index.html index.htm;|g' /etc/nginx/sites-available/default
 
-# Konfigurasi handling URL Laravel (try_files)
+# 4. FIX ROUTING (Ini yang bikin /register bisa jalan)
 sed -i 's|try_files $uri $uri/ =404;|try_files $uri $uri/ /index.php?$args;|g' /etc/nginx/sites-available/default
 
-# Restart Nginx untuk menerapkan perubahan
+# 5. Restart Nginx
 service nginx reload
 
-# Jalankan PHP-FPM (command utama container)
+# 6. Jalankan PHP
 php-fpm
