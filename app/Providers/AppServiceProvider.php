0c4acbde3eac65673720b2c2683f07224d677e81
farelmario <email@farelmario.com>
2025-12-22 14:56:09 +0700

    Upload asset build public

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,10 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        //
+{
+    // Paksa HTTPS saat di Azure (Production)
+    if(env('APP_ENV') !== 'local') {
+        \Illuminate\Support\Facades\URL::forceScheme('https');
     }
+}
 }
