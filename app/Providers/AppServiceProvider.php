ef0032db95900bbd2fdeec6c1ddb679f56b8d664
unknown <alfidianxannov@gmail.com>
2025-12-24 17:31:07 +0700

    perbaruiAppServiceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,10 +19,10 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-{
-    // Paksa HTTPS saat di Azure (Production)
-    if(env('APP_ENV') !== 'local') {
-        \Illuminate\Support\Facades\URL::forceScheme('https');
+    {
+        // Paksa HTTPS saat di Azure (Production)
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
-}
 }
