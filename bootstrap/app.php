b3f60a9532ee67bc06407431e43bda3a36472542
Alfidian <alfidianxannov@gmail.com>
2026-01-02 22:38:48 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request; // <--- 1. TAMBAHAN PENTING (Jangan dihapus)
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,12 +14,19 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         
         $middleware->web(append: [
-        \App\Http\Middleware\TrackVisitors::class,
+            \App\Http\Middleware\TrackVisitors::class,
         ]);
 
+        // --- 2. BAGIAN INI DIPERBARUI UNTUK AZURE ---
         $middleware->trustProxies(at: [
             '*',
-        ]);
+        ], headers: Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO |
+            Request::HEADER_X_FORWARDED_AWS_ELB
+        );
+        // ---------------------------------------------
         
         $middleware->alias([
             'isAdmin' => \App\Http\Middleware\IsAdmin::class,
@@ -29,6 +37,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'midtrans-callback', // Izinkan URL ini diakses tanpa token CSRF
         ]);
 
+        // (Kode duplikat Anda di bawah ini saya biarkan sesuai permintaan 'tanpa ubah kode lama')
         $middleware->alias([
             'isAdmin' => \App\Http\Middleware\IsAdmin::class,
             'isOwner' => \App\Http\Middleware\IsOwner::class,
